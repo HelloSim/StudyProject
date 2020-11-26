d5ca06bdef1078d2a81a9470739c91eff20c94a3
HelloSim <hellosim@aliyun.com>
2020-11-26 18:35:04 +0800

    打卡月记录显示方式修改，引用了一个第三方的表格框架

--- TravelTool/src/main/java/com/sim/traveltool/ui/activity/RecordAllActivity.java
+++ TravelTool/src/main/java/com/sim/traveltool/ui/activity/RecordAllActivity.java
@@ -1,16 +1,26 @@
 package com.sim.traveltool.ui.activity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.bin.david.form.core.SmartTable;
+import com.bin.david.form.data.CellInfo;
+import com.bin.david.form.data.column.Column;
+import com.bin.david.form.data.format.bg.ICellBackgroundFormat;
+import com.bin.david.form.data.format.tip.MultiLineBubbleTip;
+import com.bin.david.form.data.style.FontStyle;
+import com.bin.david.form.data.table.TableData;
+import com.bin.david.form.utils.DensityUtils;
+import com.sim.baselibrary.utils.ToastUtil;
+import com.sim.traveltool.bean.UserInfo;
 import com.sim.traveltool.db.DaKaRecordDaoUtil;
 import com.sim.sqlitelibrary.bean.DaKaRecord;
 import com.sim.traveltool.R;
-import com.sim.traveltool.adapter.RecordAdapter;
 
 import java.util.List;
 
@@ -24,22 +34,25 @@ import butterknife.ButterKnife;
  */
 public class RecordAllActivity extends BaseActivity {
 
-    @BindView(R.id.tv_now_year_and_month)
-    TextView tv_now_year_and_month;
-    @BindView(R.id.rv_data)
-    RecyclerView rv_data;
+    //    @BindView(R.id.tv_now_year_and_month)
+//    TextView tv_now_year_and_month;
+//    @BindView(R.id.rv_data)
+//    RecyclerView rv_data;
+    @BindView(R.id.table_data)
+    SmartTable<DaKaRecord> table;
 
     private String yearAndMonth;
     private String year;
     private String month;
     private List<DaKaRecord> daKaRecordList;
-    private RecordAdapter recordAdapter;
+//    private RecordAdapter recordAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_all);
         ButterKnife.bind(this);
+        FontStyle.setDefaultTextSize(DensityUtils.sp2px(this, 18)); //设置全局字体大小
         initData();
         initView();
     }
@@ -49,14 +62,75 @@ public class RecordAllActivity extends BaseActivity {
         year = yearAndMonth.split("-")[0];
         month = yearAndMonth.split("-")[1];
         daKaRecordList = DaKaRecordDaoUtil.getInstance().queryRecordForMonth(this, year, month);
-        recordAdapter = new RecordAdapter(this, daKaRecordList);
+//        recordAdapter = new RecordAdapter(this, daKaRecordList);
+
+        table.setData(daKaRecordList);
+//        table.setZoom(true, 2, 1);//设置放大最大和最小值
+        table.getConfig().setShowXSequence(false);//是否显示顶部序号列
+        table.getConfig().setShowYSequence(false);//是否显示左侧序号列
+        table.getConfig().setTableTitleStyle(new FontStyle(40, Color.WHITE));//设置表格标题字体样式
+        table.getConfig().setContentCellBackgroundFormat(new ICellBackgroundFormat<CellInfo>() {
+            @Override
+            public void drawBackground(Canvas canvas, Rect rect, CellInfo cellInfo, Paint paint) {
+                if (cellInfo.row % 2 == 0) {
+                    paint.setColor(Color.WHITE);
+                } else {
+                    paint.setColor(Color.GRAY);
+                }
+                canvas.drawRect(rect, paint);
+            }
+
+            @Override
+            public int getTextColor(CellInfo cellInfo) {
+                return 0;
+            }
+        });//设置内容格子背景格式
+        FontStyle fontStyle = new FontStyle();
+        MultiLineBubbleTip<Column> tip = new MultiLineBubbleTip<Column>(this, R.mipmap.round_rect, R.mipmap.triangle, fontStyle) {
+            @Override
+            public boolean isShowTip(Column column, int position) {
+                return true;
+            }
+
+            @Override
+            public String[] format(Column column, int position) {
+                DaKaRecord daKaRecord = daKaRecordList.get(position);
+                String[] strings = {column.getColumnName() + ":", String.valueOf(column.getDatas().get(position))};
+                return strings;
+            }
+        };
+        tip.setColorFilter(Color.parseColor("#FA8072"));
+        tip.setAlpha(0.8f);
+        table.getProvider().setTip(tip);//批注
+//        table.getTableData().setOnRowClickListener(new TableData.OnRowClickListener<DaKaRecord>() {
+//            @Override
+//            public void onClick(Column column, DaKaRecord daKaRecord, int col, int row) {
+//                switch (col) {
+//                    case 0:
+//                        ToastUtil.T_Info(RecordAllActivity.this, daKaRecord.getDay());
+//                        break;
+//                    case 1:
+//                        ToastUtil.T_Info(RecordAllActivity.this, daKaRecord.getWeek());
+//                        break;
+//                    case 2:
+//                        ToastUtil.T_Info(RecordAllActivity.this, daKaRecord.getStartTime());
+//                        break;
+//                    case 3:
+//                        ToastUtil.T_Info(RecordAllActivity.this, daKaRecord.getEndTime());
+//                        break;
+//                    case 4:
+//                        ToastUtil.T_Info(RecordAllActivity.this, daKaRecord.getOther());
+//                        break;
+//                }
+//            }
+//        });//点击事件
     }
 
     private void initView() {
-        tv_now_year_and_month.setText(yearAndMonth + "月" + getString(R.string.all_record));
+//        tv_now_year_and_month.setText(yearAndMonth + "月" + getString(R.string.all_record));
 
-        rv_data.setLayoutManager(new LinearLayoutManager(this));
-        rv_data.setAdapter(recordAdapter);
+//        rv_data.setLayoutManager(new LinearLayoutManager(this));
+//        rv_data.setAdapter(recordAdapter);
     }
 
 }
--- sqlitelibrary/src/main/java/com/sim/sqlitelibrary/bean/DaKaRecord.java
+++ sqlitelibrary/src/main/java/com/sim/sqlitelibrary/bean/DaKaRecord.java
@@ -1,5 +1,8 @@
 package com.sim.sqlitelibrary.bean;
 
+import com.bin.david.form.annotation.SmartColumn;
+import com.bin.david.form.annotation.SmartTable;
+
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Generated;
 import org.greenrobot.greendao.annotation.Id;
@@ -10,18 +13,24 @@ import org.greenrobot.greendao.annotation.Id;
  * @Description 插入数据库的打卡记录实体
  */
 @Entity
+@SmartTable(name = "打卡详情")
 public class DaKaRecord {
 
     @Id(autoincrement = true)
     private Long id;//自增长id
     private String year;//年
     private String month;//月
+    @SmartColumn(id = 1, name = "日期", fixed = true)
     private String day;//日
+    @SmartColumn(id = 2, name = "星期")
     private String week;//周几
+    @SmartColumn(id = 3, name = "上班卡时间")
     private String startTime;//上班卡时间
+    @SmartColumn(id = 4, name = "下班卡时间")
     private String endTime;//下班卡时间
     private String isLate;//是否迟到：0未迟到、1迟到
     private String isLeaveEarly;//是否早退：0未早退、1早退
+    @SmartColumn(id = 5, name = "其他")
     private String other;//其他
 //    private String isWorkingDay;//0工作日 1 假日 2节日
 
