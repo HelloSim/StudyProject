836759d775a12a5ef99eba4883b5a6a4d6b9291f
HelloSim <hellosim@aliyun.com>
2020-11-27 18:50:43 +0800

    月打卡记录页面修改（颜色显示修改待确定）

--- TravelTool/src/main/java/com/sim/traveltool/ui/activity/RecordAllActivity.java
+++ TravelTool/src/main/java/com/sim/traveltool/ui/activity/RecordAllActivity.java
@@ -5,17 +5,25 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.bin.david.form.core.SmartTable;
+import com.bin.david.form.core.TableConfig;
 import com.bin.david.form.data.CellInfo;
 import com.bin.david.form.data.column.Column;
+import com.bin.david.form.data.format.IFormat;
+import com.bin.david.form.data.format.bg.BaseCellBackgroundFormat;
 import com.bin.david.form.data.format.bg.ICellBackgroundFormat;
+import com.bin.david.form.data.format.draw.IDrawFormat;
 import com.bin.david.form.data.format.tip.MultiLineBubbleTip;
 import com.bin.david.form.data.style.FontStyle;
 import com.bin.david.form.utils.DensityUtils;
 import com.haibin.calendarview.Calendar;
+import com.sim.baselibrary.utils.TimeUtil;
 import com.sim.sqlitelibrary.bean.RecordDataBean;
 import com.sim.traveltool.R;
 import com.sim.traveltool.db.RecoedDataDaoUtil;
@@ -32,6 +40,8 @@ import butterknife.ButterKnife;
  */
 public class RecordAllActivity extends BaseActivity {
 
+    @BindView(R.id.title)
+    TextView title;
     @BindView(R.id.table_data)
     SmartTable<RecordDataBean> table;
 
@@ -55,26 +65,14 @@ public class RecordAllActivity extends BaseActivity {
     }
 
     private void initView() {
-//        table.setZoom(true, 2, 1);//设置放大最大和最小值
-        table.getConfig().setShowXSequence(false);//是否显示顶部序号列
-        table.getConfig().setShowYSequence(false);//是否显示左侧序号列
-        table.getConfig().setTableTitleStyle(new FontStyle(40, Color.WHITE));//设置表格标题字体样式
-        table.getConfig().setContentCellBackgroundFormat(new ICellBackgroundFormat<CellInfo>() {
-            @Override
-            public void drawBackground(Canvas canvas, Rect rect, CellInfo cellInfo, Paint paint) {
-                if (cellInfo.row % 2 == 0) {
-                    paint.setColor(Color.WHITE);
-                } else {
-                    paint.setColor(Color.GRAY);
-                }
-                canvas.drawRect(rect, paint);
-            }
+        title.setText(RecoedDataDaoUtil.getInstance().getMonth(this, calendar) + "月" + getString(R.string.all_record));
+        table.setZoom(true, 2, 1);//设置放大最大和最小值
+        table.getConfig()
+                .setShowXSequence(false)//是否显示顶部序号列
+                .setShowYSequence(false)//是否显示左侧序号列
+                .setShowTableTitle(false)//是否显示表格标题
+                .setTableTitleStyle(new FontStyle(40, Color.WHITE));//设置表格标题字体样式
 
-            @Override
-            public int getTextColor(CellInfo cellInfo) {
-                return 0;
-            }
-        });//设置内容格子背景格式
         FontStyle fontStyle = new FontStyle();
         MultiLineBubbleTip<Column> tip = new MultiLineBubbleTip<Column>(this, R.mipmap.round_rect, R.mipmap.triangle, fontStyle) {
             @Override
@@ -84,7 +82,7 @@ public class RecordAllActivity extends BaseActivity {
 
             @Override
             public String[] format(Column column, int position) {
-                String[] strings = {RecoedDataDaoUtil.getInstance().getYearMonth(RecordAllActivity.this, calendar)+"-"+
+                String[] strings = {RecoedDataDaoUtil.getInstance().getYearMonth(RecordAllActivity.this, calendar) + "-" +
                         table.getTableData().getColumns().get(0).getDatas().get(position) + ":",
                         String.valueOf(column.getDatas().get(position))};
                 return strings;
@@ -115,6 +113,46 @@ public class RecordAllActivity extends BaseActivity {
 //                }
 //            }
 //        });//点击事件
+        List<String> dayColum = null;
+        for (Column column : table.getTableData().getColumns()) {
+            if (column.getColumnName().equals("日期")) {
+                dayColum = column.getDatas();
+            }
+        }
+        List<String> finalDayColum = dayColum;
+        table.getConfig().setContentCellBackgroundFormat(new BaseCellBackgroundFormat<CellInfo>() {
+            @Override
+            public int getBackGroundColor(CellInfo cellInfo) {
+                if (cellInfo.value.equals("未打卡")) {
+                    switch (TimeUtil.getWeek(RecoedDataDaoUtil.getInstance().getYearMonth(RecordAllActivity.this, calendar) + "-" + finalDayColum.get(cellInfo.row))) {
+                        case "星期六":
+                        case "星期日":
+                            return TableConfig.INVALID_COLOR;
+                        default:
+                            return Color.WHITE;
+                    }
+                } else {
+                    if (cellInfo.col == 2) {
+                        int hour = Integer.parseInt(cellInfo.value.split(":", 2)[0]);
+                        int minute = Integer.parseInt(cellInfo.value.split(":", 2)[1]);
+                        if (hour > 9 || (hour == 9 && minute > 30)) {
+                            return TableConfig.INVALID_COLOR;
+                        } else {
+                            return Color.WHITE;
+                        }
+                    } else if (cellInfo.col == 3) {
+                        int hour = Integer.parseInt(cellInfo.value.split(":", 2)[0]);
+                        int minute = Integer.parseInt(cellInfo.value.split(":", 2)[1]);
+                        if (hour < 18 || (hour == 18 && minute < 30)) {
+                            return TableConfig.INVALID_COLOR;
+                        } else {
+                            return Color.WHITE;
+                        }
+                    }
+                    return TableConfig.INVALID_COLOR; //返回无效颜色，不会绘制
+                }
+            }
+        });
     }
 
 }
