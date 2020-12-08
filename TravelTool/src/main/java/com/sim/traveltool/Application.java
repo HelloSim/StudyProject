ea221449b20824efd0c3ad28ecb8ecd5abb94d1f
HelloSim <hellosim@aliyun.com>
2020-12-08 10:39:40 +0800

    新添 CrashHandler 奔溃异常处理类 新添 腾讯Bugly 异常log上报

--- TravelTool/src/main/java/com/sim/traveltool/Application.java
+++ TravelTool/src/main/java/com/sim/traveltool/Application.java
@@ -1,9 +1,16 @@
 package com.sim.traveltool;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 
+import com.sim.baselibrary.utils.CrashHandler;
 import com.sim.traveltool.internet.APIFactory;
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 /**
  * @Auther Sim
@@ -14,12 +21,24 @@ public class Application extends android.app.Application {
 
     public static Context context;
 
-    @SuppressLint("ApplySharedPref")
     @Override
     public void onCreate() {
         super.onCreate();
-        context = this;
+        context = getApplicationContext();
+        CrashHandler.getInstance().init(getApplicationContext());
+        CrashReport.setUserId(getSeries());
+        CrashReport.initCrashReport(context, AppHelper.Bugly_APPID, !BuildConfig.LOG_DEBUG);//Logcat的TAG=CrashReportInfo
         APIFactory.getInstance().init(this);
+    }
+
+    private String getSeries() {
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream("/sys/class/net/eth0/address")))) {
+            String ethernetMacAddress = input.readLine();
+            return new BigInteger(ethernetMacAddress.trim().replace(":", ""), 16).toString(10);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
 }
