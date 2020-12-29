4216f8197455ac0e0d21ba61cd5a527049ae54e1
HelloSim <hellosim@aliyun.com>
2020-12-29 18:15:53 +0800

    versionName "3.2.10" versionCode 15 封装 一些Base基本类 修改 打卡页面布局

---
+++ baselibrary/src/main/java/com/sim/baselibrary/base/BaseViewHolder.java
@@ -0,0 +1,29 @@
+package com.sim.baselibrary.base;
+
+import android.view.View;
+
+import androidx.annotation.IdRes;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @Auther Sim
+ * @Time 2020/12/29 16:12
+ * @Description BaseViewHolder
+ */
+public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+        bindViews();
+    }
+
+    protected abstract void bindViews() ;
+
+    public final <T extends View> T findViewById(@IdRes int id) {
+        if (id == View.NO_ID) {
+            return null;
+        }
+        return itemView.findViewById(id);
+    }
+
+}
